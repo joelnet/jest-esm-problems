524249aa2bf9c5b3752a6504506dd8775594d69d
Joel Thoms <jthoms@paciolan.com>
2023-03-28 17:28:05 -0700

    refactor: 💡 cleanup test code

--- src/__tests__/fetch.test.js
+++ src/__tests__/fetch.test.js
@@ -1,19 +1,22 @@
-import { jest } from '@jest/globals';
+import { jest } from "@jest/globals";
+import { Response } from "node-fetch";
 
-jest.unstable_mockModule('node-fetch', () => ({
-  default: function () { return { ok: true, json: () => ({ success: true }) } }
+jest.unstable_mockModule("node-fetch", () => ({
+  default: jest.fn(),
 }));
 
-import { Response } from 'node-fetch';
+const mockFetch = await import("node-fetch");
+
 const { fetchJson } = await import("../fetch.js");
 
 describe("fetchJson", () => {
   it("should return JSON", async () => {
     const expected = { success: true };
     const response = new Response(JSON.stringify(expected));
-    // @ts-ignore
+
+    mockFetch.default.mockReturnValueOnce(response);
 
     const actual = await fetchJson("http://fake.url/");
-    expect(actual).toMatchObject(expected);
+    expect(actual).toStrictEqual(expected);
   });
 });
